129a972bb00bcca43b550fd867fe5e1c71d442dc
Kenzie Togami <ket1999@gmail.com>
2016-04-24 12:49:45 -0400

    Python 3.

--- server.py
+++ server.py
@@ -23,7 +23,7 @@ def index():
         raise
 def root():
     if request.json is None:
-       print 'Invalid Content-Type'
+       print('Invalid Content-Type')
        return 'Content-Type must be application/json and the request body must contain valid JSON', 400
 
     if SECRET:
@@ -96,7 +96,7 @@ def post(text, url, channel):
     r = requests.post(url, headers=headers, data=json.dumps(data), verify=False)
 
     if r.status_code is not requests.codes.ok:
-        print 'Encountered error posting to Mattermost URL %s, status=%d, response_body=%s' % (url, r.status_code, r.json())
+        print('Encountered error posting to Mattermost URL %s, status=%d, response_body=%s' % (url, r.status_code, r.json()))
 
 def get_hook_info(data):
     if 'repository' in data:
